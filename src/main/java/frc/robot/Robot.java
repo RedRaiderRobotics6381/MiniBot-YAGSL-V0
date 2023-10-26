5170eafac027e672200107f39348093449046538
Jason Kreidler <jason.kreidler@gmail.com>
2023-10-26 08:15:49 -0500

    UpdateLimelightHelper

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -58,7 +58,7 @@ public class Robot extends TimedRobot
     // immediately when disabled, but then also let it be pushed more 
     disabledTimer = new Timer();
     Autos.init();
-    DriverStation.silenceJoystickConnectionWarning(true);
+    DriverStation.silenceJoystickConnectionWarning(true); // Disable joystick connection warning
   }
 
   /**
