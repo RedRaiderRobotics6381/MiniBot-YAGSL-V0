ed2936d63d867aebc8c784895f30a0d6b604f6fe
Jason Kreidler <jason.kreidler@gmail.com>
2023-11-19 17:54:05 -0600

    Update

--- src/main/java/frc/robot/commands/Arm/Rotate/ArmRotateToDrivePosCmd.java
+++ src/main/java/frc/robot/commands/Arm/Rotate/ArmRotateToDrivePosCmd.java
@@ -1,6 +1,7 @@
 package frc.robot.commands.Arm.Rotate;
 
 import edu.wpi.first.math.controller.ArmFeedforward;
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
 import frc.robot.Robot;
@@ -8,11 +9,15 @@ import frc.robot.Constants.ArmConstants;
 import frc.robot.subsystems.Secondary.ArmRotateSubsystem;
 
 public class ArmRotateToDrivePosCmd extends CommandBase {
-    // private static final double kS = 0.0;
-    // private static final double kG = 0.0;
-    // private static final double kV = 0.0;
-    // private static final double kA = 0.0;    
-    // ArmFeedforward feedforward = new ArmFeedforward(kS, kG, kV, kA);
+    private static final double kS = 0.0; //kS = static friction
+    private static final double kG = 0.0; //kG = gravity
+    private static final double kV = 0.0; //kV = velocity
+    private static final double kA = 0.0; //kA = acceleration   
+    private static final double kP = 0.0; //kP = proportional
+    private static final double kI = 0.0; //kI = integral
+    private static final double kD = 0.0; //kD = derivative
+    ArmFeedforward armFeedForward = new ArmFeedforward(kS, kG, kV, kA);
+    PIDController armPIDController = new PIDController(kP, kI, kD);
 
     double P;
 
